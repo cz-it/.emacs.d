aa57ee43ebaa350550ed3b0493a3805fa40ac748
Gotaly <gota@Gotaly.July>
2014-03-28 00:33:37 +0800

    add color-theme

---
+++ etc/init.d/theme.el
@@ -0,0 +1,5 @@
+;;for color-theme plugin
+(add-to-list 'load-path "~/.emacs.d/lib/themes/color-theme")
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-vim-colors)
--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/etc/init.d")
 (load "ide.el")
+(load "theme.el")
 (load "base.el")
 
