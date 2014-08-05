9756420d24cfc3bd24c921efb850c06ae9242cd6
C@Z <cz.theng@gmail.com>
2014-08-05 17:23:11 +0800

    f12 close dir f11 open dir

--- etc/init.d/shortcuts.el
+++ etc/init.d/shortcuts.el
@@ -5,3 +5,8 @@
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 (global-set-key (kbd "C-c <up>") 'windmove-up)
 (global-set-key (kbd "C-c <down>") 'windmove-down)
+
+
+; open and close dir-tree sr-speedbar
+(global-set-key (kbd "<f12>") 'sr-speedbar-close)
+(global-set-key (kbd "<f11>") 'sr-speedbar-open)
