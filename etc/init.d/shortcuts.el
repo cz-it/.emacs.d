237fd74b44ddbec7ac1f25afcc8f681d942c3e3e
C@Z <cz.theng@gmail.com>
2014-08-05 17:50:36 +0800

    change windows size

--- etc/init.d/shortcuts.el
+++ etc/init.d/shortcuts.el
@@ -1,10 +1,14 @@
 ;; shortcuts
 ; switch between splited windows
-
 (global-set-key (kbd "C-c <left>") 'windmove-left)
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 (global-set-key (kbd "C-c <up>") 'windmove-up)
 (global-set-key (kbd "C-c <down>") 'windmove-down)
+; change size of window
+(global-set-key (kbd "C-x <right>")  'enlarge-window-horizontally)
+(global-set-key (kbd "C-x <left>")   'shrink-window-horizontally)
+(global-set-key (kbd "C-x <up>")     'enlarge-window)
+(global-set-key (kbd "C-x <down>")   'shrink-window) 
 
 
 ; open and close dir-tree sr-speedbar
